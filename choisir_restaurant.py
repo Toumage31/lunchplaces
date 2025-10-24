2e3cf041af5d7550486ecb2e838ed47aa633b96c
Thomas Olivan <tolivan@mercator-ocean.fr>
2025-10-24 13:57:31 +0200

    first_commit

---
+++ choisir_restaurant.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import random
+
+# Liste des restaurants
+restaurants = [
+    "Viet-AN",
+    "DICAPO",
+    "Pizza du bout de la rue",
+    "Boulangerie",
+    "Choix de Victor",
+    "Minotaure",
+    "Pitaya",
+    "Boulangerie de la gare",
+]
+
+# Choix aléatoire
+choix = random.choice(restaurants)
+
+print(f"🍽️ Aujourd'hui, tu manges chez : {choix}")
